785eb6e0ba9bc495e1b1ec8b3f84e62245677a9e
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 17:47:23 +0000

    Improved custom tooltip styling

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -10,21 +10,27 @@ export default function Graph() {
         const date = new Date(dateTimeString);
         const hours = date.getHours();
         const minutes = date.getMinutes();
-    
+
         // Add leading zero if needed
         const formattedHours = hours < 10 ? `0${hours}` : hours;
         const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+        if (hours == 0 && minutes == 0) {
+            return(date.toUTCString());
+        }
     
         return `${formattedHours}:${formattedMinutes}`;
       };
 
     function CustomTooltip({ active, payload, label }) {
         if (active) {
+            const date = new Date(label);
             return (
-                <div>
-                    <h5 className="text-center">{formatTime(label)}</h5>
+                <div className="border-2 border-black rounded-md bg-blue-200 p-2">
+                    <h5 className="text-center font-semibold">{`${date.toDateString()} ${formatTime(label)}`}</h5>
+                    <div className="border border-black"></div>
                     <p><span>Upper Estimate: </span><span>{payload[0].value}</span></p>
-                    <h6><span>Mean Estimate: </span><span>{payload[1].value}</span></h6>
+                    <h6><span className="font-bold">Mean Estimate: </span><span className="font-bold">{payload[1].value}</span></h6>
                     <p><span>Lower Estimate: </span><span>{payload[2].value}</span></p>
                 </div>
             )
