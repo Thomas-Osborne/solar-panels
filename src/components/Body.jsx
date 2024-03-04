62bb61f557248d289d9fa69678c2684f675815df
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 20:10:33 +0000

    Added history component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,18 @@ import Box from './Box';
 import Calculator from './Calculator';
 import Estimates from './Estimates';
 import Graph from './Graph';
+import History from './History';
 
 export default function Body() {
     return (
         <main className="h-screen">
-            <div className="rounded-x1 m-5 px-5 border-2 border-blue-500 rounded-xl h-3/5">
-                <Box name="Graph" content={Graph} />
+            <div className="rounded-x1 m-5 px-5 border-2 border-blue-500 rounded-xl h-3/5 flex">
+                <div className="w-1/5 px-2">
+                    <Box name="Previous Records" content={History} />
+                </div>
+                <div className="w-4/5 px-2">
+                    <Box name="Graph" content={Graph} />
+                </div>
             </div>
             <div className="rounded-x1 m-5 px-5 border-2 border-blue-500 rounded-xl h-2/5 flex">
                 <div className="w-1/2 px-2">
---
+++ src/components/History.jsx
@@ -0,0 +1,7 @@
+export default function History() {
+    return (
+        <p>
+            I'm the history!
+        </p>
+    )
+}
