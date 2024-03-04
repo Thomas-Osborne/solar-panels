7bf227f1e326d359ca60ee715017acf0fb4ebf23
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 21:11:12 +0000

    Updated styling of calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,18 +2,29 @@ export default function Calculator()  {
     return (
         <div>
             <form>
-                <label>Hours Charging</label>
-                <input type="text" />
+                <div className="flex flex-col py-1">
+                    <label className="font-semibold">Hours Charging</label>
+                    <input className="bg-red-100" type="number" />
+                </div>
+                <div className="flex flex-col py-1">
+                    <label className="font-semibold">SoC Increase</label>
+                    <input className="bg-red-100" type="number" />
+                </div>
                 <br />
-                <label>SoC Increase</label>
-                <input type="text" />
-                <br />
-                <input type="submit" />
+                <div className="flex justify-center">
+                <input type="submit" className="bg-blue-200" />
+                </div>
             </form>
 
             <div>
-                <div><span>Wattage Required</span><span>Answer</span></div>
-                <div><span>Set Current Value</span><span>Answer</span></div>
+                <div className="flex flex-col py-1">
+                    <span className="font-semibold">Wattage Required</span>
+                    <span className="bg-red-100">Answer</span>
+                </div>
+                <div className="flex flex-col py-1">
+                    <span className="font-semibold">Set Current Value</span>
+                    <span className="bg-red-100">Answer</span>
+                </div>
             </div>
         </div>
     )
