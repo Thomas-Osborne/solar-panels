939e6d3e7d2b2875c28ad4654c1ace0138c529df
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 19:38:43 +0000

    Added padding to list items on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,10 +8,10 @@ export default function Sidebar() {
 
     return (
         <div className="h-screen py-3 w-50 border-r-2 border-gray-300">
-            <h2 className="font-bold text-blue-900 text-2xl">Dashboard</h2>
+            <h2 className="font-bold text-blue-900 text-2xl px-10">Dashboard</h2>
             <ul>
                 {Links.map(link => 
-                    <li className="my-2 hover:text-blue-300 hover:bg-gray-200"><a href={link.link}>{link.name}</a></li>
+                    <li className="my-2 hover:text-blue-300 hover:bg-gray-200 px-10"><a href={link.link}>{link.name}</a></li>
             )}
             </ul>
         </div>
