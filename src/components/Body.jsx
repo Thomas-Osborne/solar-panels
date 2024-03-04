17215780cd181e9523b0828d685a9abc985a9f82
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 20:27:17 +0000

    Made boxes stop overflowing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,24 +7,20 @@ import History from './History';
 export default function Body() {
     return (
         <main className="h-screen flex flex-col">
-            <div className="rounded-xl m-5 px-5 border-2 border-blue-500 h-3/5">
-                <div className="h-full flex">
-                    <div className="w-1/5 px-2">
-                        <Box name="Previous Records" content={History} />
-                    </div>
-                    <div className="w-4/5 px-2">
-                        <Box name="Graph" content={Graph} />
-                    </div>
+            <div className="rounded-xl m-5 px-5 border-2 border-blue-500 h-3/5 flex">
+                <div className="w-1/5 flex flex-col px-2">
+                    <Box name="Previous Records" content={<History />} />
+                </div>
+                <div className="w-4/5 flex flex-col px-2">
+                    <Box name="Graph" content={<Graph />} />
                 </div>
             </div>
-            <div className="rounded-xl m-5 px-5 border-2 border-blue-500 h-2/5">
-                <div className="h-full flex">
-                    <div className="w-1/2 px-2">
-                        <Box name="Calculator" content={Calculator} />  
-                    </div>
-                    <div className="w-1/2 px-2">
-                        <Box name="Estimates" content={Estimates} />
-                    </div>
+            <div className="rounded-xl m-5 px-5 border-2 border-blue-500 h-2/5 flex">
+                <div className="w-1/2 flex flex-col px-2">
+                    <Box name="Calculator" content={<Calculator />} />  
+                </div>
+                <div className="w-1/2 flex flex-col px-2">
+                    <Box name="Estimates" content={<Estimates />} />
                 </div>
             </div>
       </main>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,12 +1,9 @@
 export default function Box(props) {
-
-    const Content = props.content;
-
     return (
         <div className="rounded-xl p-5 my-5 border-2 border-rose-500 h-full">
             <h3 className="text-gray-500 text-xl font-semibold">{props.name}</h3>
             <div className="h-full overflow-y-auto">
-                <Content />
+                {props.content}
             </div>
         </div>
     )
