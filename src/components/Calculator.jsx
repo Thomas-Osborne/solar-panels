e8378c98b8b5fe4f7c3ddf6f5746fe948a2bdaab
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 15:05:23 +0000

    Added calculator form details

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,20 @@
 export default function Calculator()  {
     return (
-        <p>I'm a calculator!</p>
+        <div>
+            <form>
+                <label>Hours Charging</label>
+                <input type="text" />
+                <br />
+                <label>SoC Increase</label>
+                <input type="text" />
+                <br />
+                <input type="submit" />
+            </form>
+
+            <div>
+                <div><span>Wattage Required</span><span>Answer</span></div>
+                <div><span>Set Current Value</span><span>Answer</span></div>
+            </div>
+        </div>
     )
 }
