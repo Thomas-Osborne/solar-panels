7908b5a020e42b7928cd384bc72bbe532b74d6c3
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 11:38:40 +0000

    Added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Body from './components/Body';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 export default function App() {
 
   return (
     <>
       <Header />
+      <Sidebar />
       <Body />
       <Footer />
     </>
