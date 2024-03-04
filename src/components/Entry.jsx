5e6c08d686408616901c4484f970822dad082994
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-04 20:48:31 +0000

    Made entry buttons cleaner

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,5 +1,9 @@
 export default function Entry(props) {
     return (
-        <p>I'm the entry {props.name}!</p>
+        <div>
+        <button className="bg-blue-500 text-white text-left p-3 my-1 w-full border rounded-md hover:bg-blue-600">
+            <h1 className="font-semibold">{props.date}</h1>
+        </button>
+        </div>
     )
 }
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -2,11 +2,11 @@ import Entry from "./Entry";
 
 export default function History() {
 
-    const Entries = [{name: "First"}, {name: "Second"}, {name: "Third"}];
+    const Entries = [{date: "First"}, {date: "Second"}, {date: "Third"}];
 
     return (
         <div>
-            {Entries.map(entry => <Entry name={entry.name}/>)}
+            {Entries.map(entry => <Entry date={entry.date}/>)}
         </div>
     )
 }
