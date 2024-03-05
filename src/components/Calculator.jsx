5ea4a47577e64c961691462b4811e444f0358cae
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-05 13:25:46 +0000

    Update calculated values correctly

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,21 +3,55 @@ import React from "react";
 export default function Calculator()  {
 
     const [formData, setFormData] = React.useState(
-        {chargingHours: "", desiredIncrease: ""}
+        {
+            chargingHours: 0,
+            desiredIncrease: 0
+        }
     )
 
-    const [requiredKwh, setRequiredKwh] = React.useState("0");
-    const [currentValue, setCurrentValue] = React.useState("0");
+    console.log(formData);
+
+    const [calculations, setCalculations] = React.useState(
+        {
+            requiredKwh: 0,
+            currentValue: 0
+        }
+    )
 
     function handleChange(event) {
+
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
                 [event.target.name]: event.target.value
             }
         })
+
+        setCalculations(
+            calculateValues(
+                event.target.name === 'chargingHours' ? event.target.value : formData.chargingHours,
+                event.target.name === 'desiredIncrease' ? event.target.value : formData.desiredIncrease
+        ));
     }
 
+
+    function calculateValues(chargingHours, desiredIncrease) {
+        const AV_BATTERY_VOLTAGE = 52;
+        const MAX_CURRENT = 70;
+        const MAX_CHARGING_HOURS = 6;
+        const TOTAL_BATTERY_CAPACITY = 24.3;
+
+        let newRequiredKwh = (TOTAL_BATTERY_CAPACITY) * (desiredIncrease) / 100;
+        let newKwPerHourRate = (newRequiredKwh) / Math.min(chargingHours, MAX_CHARGING_HOURS);
+        let newCurrent = Math.min(newKwPerHourRate * 1000 / AV_BATTERY_VOLTAGE, MAX_CURRENT);
+
+        return {
+            requiredKwh: newRequiredKwh,
+            currentValue: newCurrent
+        };
+    }
+
+    
     return (
         <div>
             <form>
@@ -29,6 +63,7 @@ export default function Calculator()  {
                         placeholder="Hours for charging..."
                         onChange={handleChange}    
                         name="chargingHours"
+                        value={formData.chargingHours}
                     />
                 </div>
                 <div className="flex flex-col py-1">
@@ -39,6 +74,7 @@ export default function Calculator()  {
                         placeholder="Increase wanted..."
                         onChange={handleChange}
                         name="desiredIncrease"
+                        value={formData.desiredIncrease}
                     />
                 </div>
                 <br />
@@ -50,11 +86,11 @@ export default function Calculator()  {
             <div>
                 <div className="flex flex-col py-1">
                     <span className="font-semibold">Required kWh</span>
-                    <span className="bg-blue-200 text-xl">{requiredKwh}</span>
+                    <span className="bg-blue-200 text-xl">{calculations.requiredKwh.toFixed(2)} kWh</span>
                 </div>
                 <div className="flex flex-col py-1">
                     <span className="font-semibold">Set Current Value</span>
-                    <span className="bg-blue-200 text-xl">{currentValue}</span>
+                    <span className="bg-blue-200 text-xl">{calculations.currentValue.toFixed(2)} A</span>
                 </div>
             </div>
         </div>
