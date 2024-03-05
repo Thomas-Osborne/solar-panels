2a7a9f7a220ebfb8c4fe918f68fe06d1becf2bba
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-05 12:47:46 +0000

    Added state for variables

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export default function Calculator()  {
+
+    const [chargingHours, setChargingHours] = useState("");
+    const [desiredIncrease, setDesiredIncrease] = useState("");
+    const [requiredKwh, setRequiredKwh] = useState("0");
+    const [currentValue, setCurrentValue] = useState("0");
+
     return (
         <div>
             <form>
@@ -15,15 +23,15 @@ export default function Calculator()  {
                     <input type="submit"  className="bg-blue-500 text-white text-xl font-medium px-10 py-3 mb-1 mt-1 rounded-lg hover:bg-blue-600" />
                 </div>
             </form>
-            
+
             <div>
                 <div className="flex flex-col py-1">
                     <span className="font-semibold">Required kWh</span>
-                    <span className="bg-blue-200 text-xl">Answer</span>
+                    <span className="bg-blue-200 text-xl">{requiredKwh}</span>
                 </div>
                 <div className="flex flex-col py-1">
                     <span className="font-semibold">Set Current Value</span>
-                    <span className="bg-blue-200 text-xl">Answer</span>
+                    <span className="bg-blue-200 text-xl">{currentValue}</span>
                 </div>
             </div>
         </div>
