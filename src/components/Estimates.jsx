2ad61e800160e70770f63080176e875ce84b7307
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-06 12:53:24 +0000

    Fixed bug with updating daily yield

--- src/components/Estimates.jsx
+++ src/components/Estimates.jsx
@@ -9,58 +9,18 @@ export default function Estimates(props) {
     const DAILY_USAGE = 18;
 
     let desiredIncrease;
-    let chargingHours;
+    let chargingHours = 6;
 
     const forecasts = props.data[props.chosenDate];
-    let times = []
-    let index;
 
-    const currentTime = new Date();
+    const expectedYield = estimatePv(forecasts);
 
-    let analysedData = [];
-
-    for (let i = 0; i < forecasts.length; i++) {
-        forecasts[i].period_end = new Date(forecasts[i].period_end); // make it a date object for ease
-        times.push(forecasts[i].period_end.getTime());
-    }
-
-    const today = forecasts[0].period_end;
-
-    const todaysDate = forecasts[0].period_end.getUTCDate();
-    const tomorrowsDate = today.getDate() + 1; // DOES NOT WORK FOR END OF MONTHS
-
-
-    if (currentTime.getHours() <= 5) {
-        chargingHours = MAX_CHARGING_HOURS - currentTime.getHours() - 1;
-
-        for (let i = 0; i < forecasts.length; i++) {
-            if (forecasts[i].period_end.getUTCDate() === todaysDate) {
-                analysedData.push(forecasts[i]);
-            } else {
-            break;
-            }
-        }
-        index = 0;
-    } else {
-        chargingHours = MAX_CHARGING_HOURS;
-        for (let i = 0; i < forecasts.length; i++) {
-            if (forecasts[i].period_end.getUTCDate() === todaysDate) {
-                continue;
-            } else if (forecasts[i].period_end.getUTCDate() === tomorrowsDate) {
-                analysedData.push(forecasts[i]);
-            } else {
-                break;
-            }
-        }
-    }
-
-    const expectedYield = estimatePv(analysedData);
-
-    desiredIncrease = estimateDesiredIncrease(analysedData, 6, 10);
+    desiredIncrease = estimateDesiredIncrease(forecasts, 6, 10);
 
     const calculatedValues = calculateValues(chargingHours, desiredIncrease);
         
     function estimatePv(data) {
+        console.log(data);
         // use trapezium rule
         const TIME_GRANULARITY = 0.5; // should just find amount of mins between times instead
         let estimate = 0;
