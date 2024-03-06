788d8ac1d3e15c9f84642822cdbeeed7d4e936fd
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-06 17:48:18 +0000

    Finished api requests

--- backend/controllers/forecastController.js
+++ backend/controllers/forecastController.js
@@ -22,4 +22,48 @@ async function getForecast(req, res) {
     return res.status(200).json(forecast);
 }
 
-module.exports = { getForecasts, getForecast };
+async function createForecast(req, res) {
+    const { forecasts } = req.body
+
+    try {
+        const forecast = await Forecast.create({ forecasts });
+        return res.status(200).json(forecast);
+    } catch {
+        return res.status(400).json({error: error.message});
+    }
+}
+
+async function deleteForecast(req, res) {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'Forecast does not exist.'});
+    }
+
+    const forecast = await Forecast.findOneAndDelete({ _id: id });
+
+    if (!forecast) {
+        return res.status(404).json({error: 'Forecast does not exist.'});
+    }
+    
+    return res.status(200).json(forecast);
+
+}
+
+async function updateForecast(req, res) {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'Forecast does not exist.'});
+    }
+
+    const forecast = await Forecast.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    });
+
+    if (!forecast) {
+        return res.status(404).json({error: 'Forecast does not exist.'});
+    }
+}
+
+module.exports = { getForecasts, getForecast, createForecast, deleteForecast, updateForecast };
--- backend/routes/forecasts.js
+++ backend/routes/forecasts.js
@@ -1,28 +1,15 @@
 const express = require('express');
-const Forecast = require('../models/Forecast');
 const router = express.Router();
-const { getForecasts, getForecast } = require('../controllers/forecastController');
+const { getForecasts, getForecast, createForecast, deleteForecast, updateForecast } = require('../controllers/forecastController');
 
 router.get('/', getForecasts);
 
 router.get('/:id', getForecast);
 
-router.post('/', async (req, res) => {
-    const { forecasts } = req.body
-    try {
-        const forecast = await Forecast.create({ forecasts });
-        res.status(200).json(forecast);
-    } catch {
-        res.status(400).json({error: error.message});
-    }
-})
+router.post('/', createForecast);
 
-router.delete('/:id', (req, res) => {
-    res.json({message: 'DELETE a single Forecast'});
-})
+router.delete('/:id', deleteForecast);
 
-router.patch('/:id', (req, res) => {
-    res.json({message: 'UPDATE a single Forecast'});
-})
+router.patch('/:id', updateForecast);
 
 module.exports = router;
