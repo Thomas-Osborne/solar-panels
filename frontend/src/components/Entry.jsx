889ade9d8fee1338390fb1167ae92e9a7968e53b
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-07 19:59:27 +0000

    Added delete button to each entry

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -1,12 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
+
 export default function Entry(props) {
     const lastUpdated = new Date(props.data.updatedAt).toLocaleString();
     return (
-        <div>
+        <div className="bg-blue-500 text-white text-left p-3 my-1 w-full border rounded-md hover:bg-blue-600">
         <button 
-            className="bg-blue-500 text-white text-left p-3 my-1 w-full border rounded-md hover:bg-blue-600"
+            className=""
             onClick={() => props.handleClick(props.data.forecasts)}
         >
             <h1 className="font-semibold">{lastUpdated}</h1>
+        </button>
+        <button
+            className=""
+            onClick={() => console.log("You clicked me!")}>
+            <i className="pr-1 w-6"><FontAwesomeIcon icon={faDeleteLeft} fixedWidth /></i>
         </button>
         </div>
     )
