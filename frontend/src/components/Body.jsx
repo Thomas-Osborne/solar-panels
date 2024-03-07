6f8847bc0ddcd970d6453e26837d3112701f04bf
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-07 18:11:13 +0000

    Initial graph shows most recent data

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -3,57 +3,58 @@ import Calculator from './Calculator';
 import Estimates from './Estimates';
 import Graph from './Graph';
 import History from './History';
-
 import { determineFixedDate, splitData } from '../../../backend/controllers/splitDataController';
-
-import forecastsData from '../../forecasts.json';
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Body() {
+    const [allData, setAllData] = useState([]);
+    const [initialData, setInitialData] = useState(null);
+    const [dates, setDates] = useState([]);
+    const [chosenDate, setChosenDate] = useState("");
+    const [forecasts, setForecasts] = useState({});
 
-    const [allData, setAllData] = React.useState([{_id: 1, forecasts: forecastsData.forecasts, date: formatDatetime(forecastsData.forecasts[0].period_end)}]);
-
-    React.useEffect(() => {
+    useEffect(() => {
         fetchForecasts();
     }, []);
 
     async function fetchForecasts() {
-        const res = await fetch('http://localhost:4000/api/forecasts');
-        const json = await res.json();
-
-        if (res.ok) {
+        try {
+            const res = await fetch('http://localhost:4000/api/forecasts');
+            if (!res.ok) {
+                throw new Error('Failed to fetch forecasts');
+            }
+            const json = await res.json();
             setAllData(json);
+        } catch (error) {
+            console.error('Error fetching forecasts:', error);
         }
     }
 
-    function formatDatetime(date) {
-        const d = new Date(date);
-        return d.toLocaleString('en-GB', {day: "2-digit", month: "2-digit", year: "2-digit", hour: "2-digit", minute: "2-digit"})
-    }
-      
-    const initialData = allData[0]; // sorted by descending order
+    useEffect(() => {
+        if (allData.length > 0) {
+            const newInitialData = allData[0];
+            setInitialData(newInitialData);
 
-    const initialKeys = Object.keys(splitData(initialData.forecasts));
-    const initialDate = determineFixedDate(initialData.forecasts);
+            const newDates = Object.keys(splitData(newInitialData.forecasts));
+            setDates(newDates);
 
-    const [dates, setDates] = React.useState(initialKeys);
-    const [chosenDate, setChosenDate] = React.useState(initialDate);
-
-    const [forecasts, setForecasts] = React.useState(splitData(initialData.forecasts));
+            const newChosenDate = determineFixedDate(newInitialData.forecasts);
+            setChosenDate(newChosenDate);
+            
+            setForecasts(splitData(newInitialData.forecasts));
+        }
+    }, [allData]);
 
     function updateDate(date) {
         setChosenDate(date);
     }
 
     function updateForecasts(data) {
-        const newData = data;
-        const newDates = Object.keys(splitData(newData))
-        const newDate = determineFixedDate(newData);
-
+        const newDates = Object.keys(splitData(data));
+        const newDate = determineFixedDate(data);
         setDates(newDates);
         setChosenDate(newDate);
-        setForecasts(splitData(newData));
+        setForecasts(splitData(data));
     }
 
     return (
@@ -63,17 +64,17 @@ export default function Body() {
                     <Box name="Previous Records" content={<History oldData={allData} handleClick={updateForecasts} />} />
                 </div>
                 <div className="w-4/5 flex flex-col px-2">
-                    <Box name="Graph" content={<Graph data={forecasts} dates={dates} chosenDate={chosenDate} updatedAt={initialData.updatedAt} handleFetchClick={fetchForecasts} handleDateClick={updateDate}/>} />
+                    <Box name="Graph" content={<Graph data={forecasts} dates={dates} chosenDate={chosenDate} updatedAt={initialData?.updatedAt} handleFetchClick={fetchForecasts} handleDateClick={updateDate}/>} />
                 </div>
             </div>
             <div className="rounded-xl mx-5 my-1 px-5 h-2/5 flex">
                 <div className="w-1/2 flex flex-col px-2">
                     <Box name="Calculator" content={<Calculator />} />  
                 </div>
-                <div className="w-1/2 flex flex-col px-2">
-                    <Box name="Estimates" content={<Estimates data={forecasts} chosenDate={chosenDate} handleClick={updateDate}/>} />
-                </div>
+                {/* <div className="w-1/2 flex flex-col px-2">
+                    <Box name="Estimates" content={<Estimates data={forecasts} chosenDate={chosenDate} handleClick={updateDate}/>} /> 
+                </div> */}
             </div>
-      </main>
-    )
+        </main>
+    );
 }
