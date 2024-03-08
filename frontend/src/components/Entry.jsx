e30e25e428116957085b4ee6987aef287c1044ec
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-08 10:31:28 +0000

    Used flex to make delete buttons go to right

--- frontend/src/components/Entry.jsx
+++ frontend/src/components/Entry.jsx
@@ -15,7 +15,7 @@ export default function Entry(props) {
 
     const lastUpdated = new Date(props.data.updatedAt).toLocaleString();
     return (
-        <div className="bg-blue-500 text-white text-left p-3 my-1 w-full border rounded-md hover:bg-blue-600">
+        <div className="bg-blue-500 text-white text-left p-3 my-1 w-full border rounded-md flex justify-between hover:bg-blue-600">
         <button 
             className=""
             onClick={() => props.handleClick(props.data.forecasts)}
