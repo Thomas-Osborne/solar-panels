06939fd3fbd6165207ea4b2ea91c33ae3dde986a
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-08 13:29:10 +0000

    Tidied up footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer() {
     return (
-        <div className="text-xs text-center py-5 text-gray-500">
-            <p>This is the footer.</p>
-        </div>
+        <footer className="text-xs text-center py-5 text-gray-500">
+            <p>Created by Tom Osborne 2024</p>
+            <div className="container mx-auto flex justify-center gap-4 py-2">
+                <a href="/" className="hover:text-gray-400 hover:underline">Home</a>
+                <a href="https://www.github.com/Thomas-Osborne" className="hover:text-gray-400 hover:underline" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/tom-osborne-716619288/" className="hover:text-gray-400 hover:underline" target="_blank">LinkedIn</a>
+                <a href="https://www.solcast.com" className="hover:text-gray-400 hover:underline" target="_blank">Solar Irradiance Data</a>
+            </div>
+        </footer>
     )
 }
