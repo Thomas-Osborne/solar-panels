7af29fbc047dc0987ff2852c7ffb2d09d7ba7c6f
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-09 18:04:13 +0000

    Added state variable to check if logged in.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import React from 'react';
 
 export default function App() {
 
+
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
   const [isOpen, setIsOpen] = React.useState(false);
 
 
@@ -21,17 +23,19 @@ export default function App() {
 
   return (
     <>
-      <div>
-        <Logout isOpen={isOpen} openModal={openModal} closeModal={closeModal}/>
-        <Header />
-        <div className="flex">
-          <Sidebar openModal={openModal}/>
-          <div className="grow">
-            <Body />
-            <Footer />
+      {isLoggedIn
+        ? <div>
+            <Logout isOpen={isOpen} openModal={openModal} closeModal={closeModal}/>
+            <Header />
+            <div className="flex">
+              <Sidebar openModal={openModal}/>
+              <div className="grow">
+                <Body />
+                <Footer />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        : <div> Please sign in. </div>}
     </>
   )
 }
